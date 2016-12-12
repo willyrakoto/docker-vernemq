61127d8339e1ced75c0da39b1c7906d5f1cf7dc1
willyrakoto <willy.rakotoarisoa@gmail.com>
2016-12-12 11:09:08 +0100

    Change port mqtt

--- bin/vernemq.sh
+++ bin/vernemq.sh
@@ -35,7 +35,7 @@ fi
 
 echo "erlang.distribution.port_range.minimum = 9100" >> /etc/vernemq/vernemq.conf
 echo "erlang.distribution.port_range.maximum = 9109" >> /etc/vernemq/vernemq.conf
-echo "listener.tcp.default = ${IP_ADDRESS}:1883" >> /etc/vernemq/vernemq.conf
+echo "listener.tcp.default = ${IP_ADDRESS}:8081" >> /etc/vernemq/vernemq.conf
 echo "listener.ws.default = ${IP_ADDRESS}:8080" >> /etc/vernemq/vernemq.conf
 echo "listener.vmq.clustering = ${IP_ADDRESS}:44053" >> /etc/vernemq/vernemq.conf
 echo "listener.http.metrics = ${IP_ADDRESS}:8888" >> /etc/vernemq/vernemq.conf
